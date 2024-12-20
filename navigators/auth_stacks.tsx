c85dfa709b959b9804d34026ff196395bfd88c45
NGUYEN TRONG DU <ndu@infinitalk.onmicrosoft.com>
2024-12-20 13:58:13 +0900

    test: fixed eslint

--- navigators/auth_stacks.tsx
+++ navigators/auth_stacks.tsx
@@ -1,4 +1,5 @@
 import { createNativeStackNavigator } from '@react-navigation/native-stack'
+
 import Login from '../screens/Login'
 import { AuthStacksParamList } from '../types/navigators'
 
--- utils/token_storage.ts
+++ utils/token_storage.ts
@@ -1,4 +1,5 @@
 import AsyncStorage from '@react-native-async-storage/async-storage'
+
 import { TOKEN_KEYS } from '../constants'
 import { TokenBulk } from '../types'
 
